ce92060947f6e5144f768cd1ee32b1228eff8364
dev-rome <haynesjerome@gmail.com>
2023-04-14 13:53:19 -0400

    created hero section component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="h-[85vh] overflow-hidden">
+      <div className="clip-path-slant-down bg-gray-100 h-full">
+        
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
